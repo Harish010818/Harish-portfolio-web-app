9a863fcba964c5e2a7347fe40b855818ed3148a0
Harish Juyal <devharishjuyal18@gmail.com>
2025-08-17 22:42:18 +0530

    new project added to project section

--- client/src/components/sections/Projects.js
+++ client/src/components/sections/Projects.js
@@ -1,11 +1,11 @@
 
 const projects = [
   {
-    id: "space-exploration",
-    title: "Space Exploration Site",
-    desc: "This project is a visually stunning and interactive website built using React.js, React Router, and Tailwind CSS. Explore captivating destinations, stunning visuals, and engaging content about space exploration.",
-    url: "https://space-travel-website-beta.vercel.app/",
-    thumbnail: "/assets/Projects/space_home.png",
+    id: "chat-app",
+    title: "Real-time Chat Application",
+    desc: "A responsive real-time chat application built using the MERN stack (MongoDB, Express.js, React, Node.js) with Socket.io for instant communication and JWT authentication for secure login. Users can chat in real-time, just like WhatsApp or Messenger.",
+    url: "https://chat-web-app-h018.vercel.app/",
+    thumbnail: "/assets/Projects/chat_home.png"
   },
   {
     id: "todo-web-app",
@@ -13,14 +13,23 @@ const projects = [
     desc: "MERN To-Do Web App: A responsive and feature-rich To-Do app with user authentication (JWT-secured), full CRUD operations, and a sleek Tailwind CSS UI",
     url: "https://todo-web-app-r192.vercel.app/",
     thumbnail: "/assets/Projects/todo_home.png",
+  }, 
+
+  {
+    id: "space-exploration",
+    title: "Space Exploration Site",
+    desc: "This project is a visually stunning and responsive website built using React.js, React Router, and Tailwind CSS. Explore captivating destinations, stunning visuals, and engaging content about space exploration.",
+    url: "https://space-travel-website-beta.vercel.app/",
+    thumbnail: "/assets/Projects/space_home.png",
   },
   {
     id: "meme-generator",
     title: "Meme Generator App",
     desc: "This web application allows users to create custom memes by overlaying text on randomly fetched meme images. Built with React, this meme generator provides a fun and easy way for users to generate their own memes in just a few clicks.",
     url: "http://meme-generator-web-app.vercel.app/",
-    thumbnail: "/assets/Projects/meme_page.png",
-  },
+    thumbnail: "/assets/Projects/meme_home.png",
+  }
+
 ];
 
 const Projects = () => {
@@ -33,9 +42,9 @@ const Projects = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-4">
         {projects.map((project) => (
-          <a key={project.id} href={project.url} target="_blank" rel="noopener noreferrer" className="overflow-hidden hover:scale-105 transition-all duration-300 group">
-               <img src={project.thumbnail} alt={project.title} className="w-full h-[40vh] object-cover" />
-               <div className="border border-gray-800 px-2 pb-2 md:h-50">
+          <a key={project.id} href={project.url} target="_blank" rel="noopener noreferrer" className="overflow-hidden hover:scale-105 transition-all duration-300 group shadow shadow-gray-600">
+               <img src={project.thumbnail} alt={project.title} className="" />
+               <div className=" px-2 pb-2 md:h-50">
                <h2 className="text-xl font-semibold group-hover:text-[var(--office-green)]">{project.title}</h2>
                <p className="text-gray-600 mt-2">{project.desc}</p>
             </div>
