b20c2bba5d94fcc275011b3439b1810df82a774a
Harish Juyal <devharishjuyal18@gmail.com>
2025-09-27 02:12:17 +0530

    project section updated

--- client/src/components/sections/Projects.js
+++ client/src/components/sections/Projects.js
@@ -7,14 +7,7 @@ const projects = [
     url: "https://chat-web-app-h018.vercel.app/",
     thumbnail: "/assets/Projects/chat_home.png"
   },
-  {
-    id: "todo-web-app",
-    title: "Todo-web-app",
-    desc: "MERN To-Do Web App: A responsive and feature-rich To-Do app with user authentication (JWT-secured), full CRUD operations, and a sleek Tailwind CSS UI",
-    url: "https://todo-web-app-r192.vercel.app/",
-    thumbnail: "/assets/Projects/todo_home.png",
-  }, 
-
+  
   {
     id: "space-exploration",
     title: "Space Exploration Site",
@@ -23,12 +16,20 @@ const projects = [
     thumbnail: "/assets/Projects/space_home.png",
   },
   {
-    id: "meme-generator",
-    title: "Meme Generator App",
-    desc: "This web application allows users to create custom memes by overlaying text on randomly fetched meme images. Built with React, this meme generator provides a fun and easy way for users to generate their own memes in just a few clicks.",
-    url: "http://meme-generator-web-app.vercel.app/",
-    thumbnail: "/assets/Projects/meme_home.png",
-  }
+    id: "api-proxy",
+    title: "API Proxy Serve",
+    desc: "Built a Node.js + Express proxy server supporting HTTP methods, cookie-based authentication, and in-memory caching. Developed a React-based UI for sending requests and viewing responses, inspired by API testing tools like Postman.",
+    url: "https://postman-lite-nfhf.vercel.app",
+    thumbnail: "/assets/Projects/api_proxy.png",
+  },
+
+  {
+    id: "todo-web-app",
+    title: "Todo-web-app",
+    desc: "MERN To-Do Web App: A responsive and feature-rich To-Do app with user authentication (JWT-secured), full CRUD operations, and a sleek Tailwind CSS UI",
+    url: "https://todo-web-app-r192.vercel.app/",
+    thumbnail: "/assets/Projects/todo_home.png",
+  }, 
 
 ];
 
@@ -43,7 +44,7 @@ const Projects = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-4">
         {projects.map((project) => (
           <a key={project.id} href={project.url} target="_blank" rel="noopener noreferrer" className="overflow-hidden hover:scale-105 transition-all duration-300 group shadow shadow-gray-600">
-               <img src={project.thumbnail} alt={project.title} className="" />
+               <img src={project.thumbnail} alt={project.title} className=" border border-gray-400" />
                <div className=" px-2 pb-2 md:h-50">
                <h2 className="text-xl font-semibold group-hover:text-[var(--office-green)]">{project.title}</h2>
                <p className="text-gray-600 mt-2">{project.desc}</p>
