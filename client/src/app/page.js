23dcd4412012c811e5851e09fc34683de570d85f
Harish Juyal <devharishjuyal18@gmail.com>
2025-09-27 21:40:34 +0530

    main page updated

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,9 @@
 import Home from "@/components/sections/Home";
 import About from "@/components/sections/About";
-import Services from "@/components/sections/Services";
 import Projects from "@/components/sections/Projects";
 import Contact from "@/components/sections/Contact";
 import SmoothScrollWrapper from "@/components/wrapper/SmoothScrollWrapper";
+import Techstacks from "@/components/sections/TechStacks";
 
 
 const Children = () => {
@@ -12,7 +12,7 @@ const Children = () => {
       <main>
           <SmoothScrollWrapper><section id="home"><Home /></section></SmoothScrollWrapper>
           <SmoothScrollWrapper><section id="about"><About /></section></SmoothScrollWrapper>
-          <SmoothScrollWrapper><section id="services"><Services /></section></SmoothScrollWrapper>
+          <SmoothScrollWrapper><section id="tech-stack"><Techstacks /></section></SmoothScrollWrapper>
           <SmoothScrollWrapper><section id="projects"><Projects /></section></SmoothScrollWrapper>
           <SmoothScrollWrapper><section id="contact"><Contact /></section></SmoothScrollWrapper>  
       </main>
